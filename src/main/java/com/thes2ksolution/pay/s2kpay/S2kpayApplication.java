1d8b04165f89ac4e9907ef1a6d3d7a178c0b9185
Seydou <nanouroukou2075@gmail.com>
2022-12-10 11:10:43 +0000

    first commit

---
+++ src/main/java/com/thes2ksolution/pay/s2kpay/S2kpayApplication.java
@@ -0,0 +1,13 @@
+package com.thes2ksolution.pay.s2kpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2kpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S2kpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thes2ksolution/pay/s2kpay/S2kpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thes2ksolution.pay.s2kpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S2kpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
